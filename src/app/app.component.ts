3ad041101747c557c128c6db0f392cb3c2102df8
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-10 15:34:25 +0300

    modified src/app/app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-    quotes: string[];
-    quote='Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy';
+    quotes =[
+      new Quote(1,'Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy'),
+      new Quote(2,'Fake it till you make it'),
+      new Quote(3,'Create the highest vision possible for your life,because you become what you believe'),
+      new Quote(4,'A great attitude becomes a great day which becomes a great month which becomes a great year and eventually becomes a great life'),
+      
+    ]
 
   constructor(){
    
-    this.quotes = ["Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy","Fake it till you make it","Create the highest vision possible for your life,because you become what you believe","A great attitude becomes a great day which becomes a great month which becomes a great year and eventually becomes a great life"];
   }
 }
 
