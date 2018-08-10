bb7aca19b846361b7b967dbe3e1b442a6034954e
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-10 15:45:52 +0300

    modified component quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  
+  quotes =[
+    new Quote(1,'Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy'),
+    new Quote(2,'Fake it till you make it'),
+    new Quote(3,'Create the highest vision possible for your life,because you become what you believe'),
+    new Quote(4,'A great attitude becomes a great day which becomes a great month which becomes a great year and eventually becomes a great life'),
+    
+  ]
+  
   constructor() { }
-
+  
   ngOnInit() {
   }
-
+  
 }
