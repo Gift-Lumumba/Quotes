7bae52078ac6e19ad64ed570758fe6543957b7c7
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-10 17:16:54 +0300

    modified quoteComponent

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
   
   quotes =[
-    new Quote(1,'Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy'),
-    new Quote(2,'Fake it till you make it'),
-    new Quote(3,'Create the highest vision possible for your life,because you become what you believe'),
-    new Quote(4,'A great attitude becomes a great day which becomes a great month which becomes a great year and eventually becomes a great life'),
+    new Quote(1,'Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy','By:Stanley McShane'),
+    new Quote(2,'Fake it till you make it','African Contemporary Quote'),
+    new Quote(3,'Create the highest vision possible for your life,because you become what you believe','By:Owen Muriithi-Professor'),
+    new Quote(4,'A great attitude becomes a great day which becomes a great month which becomes a great year and eventually becomes a great life','Quite an inspiring quote'),
     
   ]
   
