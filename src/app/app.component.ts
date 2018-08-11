86e483c0fbf2f064ce5a7bcea8c38ec5ca482fe3
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-11 22:06:09 +0300

    modified components and added css styles to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import {Quote} from './quote';
 })
 export class AppComponent {
   quotes = [
-    new Quote(1, 'Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy', 'By Johan McGinn',new Date(2018,8,6)),
-    new Quote(2, 'Fake it till you make it', 'By Thomas Daniels',new Date(2018,8,7)),
-    new Quote(3, 'Create the highest vision possible for your life,because you become what you believe', 'By Owen Muriithi',new Date(2018,8,8)),
-    new Quote(4, 'Only I can change my life.No one can do it for me', 'By Didier Deschamps',new Date(2018,8,9)),
+    new Quote(1, 'Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy', 'By Johan McGinn',new Date(2018,7,6)),
+    new Quote(2, 'Fake it till you make it', 'By Thomas Daniels',new Date(2018,7,7)),
+    new Quote(3, 'Create the highest vision possible for your life,because you become what you believe', 'By Owen Muriithi',new Date(2018,7,8)),
+    new Quote(4, 'Only I can change my life.No one can do it for me', 'By Didier Deschamps',new Date(2018,7,9)),
   ]
 }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
   
   quotes = [
-    new Quote(1, 'Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy', 'By Johan McGinn',new Date(2018,8,6)),
-    new Quote(2, 'Fake it till you make it', 'By Thomas Daniels',new Date(2018,8,7)),
-    new Quote(3, 'Create the highest vision possible for your life,because you become what you believe', 'By Owen Muriithi',new Date(2018,8,8)),
-    new Quote(4, 'Only I can change my life.No one can do it for me', 'By Didier Deschamps',new Date(2018,8,9)),
+    new Quote(1, 'Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy', 'By:Johan McGinn',new Date(2018,7,6)),
+    new Quote(2, 'Good,better,best.Never let it rest.Till your good is better and your better is best', 'By:St.Jerome',new Date(2018,7,7)),
+    new Quote(3, 'Create the highest vision possible for your life,because you become what you believe', 'By:Owen Muriithi',new Date(2018,7,8)),
+    new Quote(4, 'Failure will never overtake me if my determination to succeed is strong enough', 'By:Og Mandino',new Date(2018,7,9)),
   ]
   
   deleteQuote(isComplete,index){
