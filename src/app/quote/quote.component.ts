46f8626276cbc7d92739d59b6d89ce9d49e9998d
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-12 17:06:39 +0300

    modified and styled components

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,15 +9,15 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
   
   quotes = [
-    new Quote(1, 'Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy', 'By:Johan McGinn',new Date(2018,7,6)),
-    new Quote(2, 'Good,better,best.Never let it rest.Till your good is better and your better is best', 'By:St.Jerome',new Date(2018,7,7)),
-    new Quote(3, 'Create the highest vision possible for your life,because you become what you believe', 'By:Owen Muriithi',new Date(2018,7,8)),
-    new Quote(4, 'Failure will never overtake me if my determination to succeed is strong enough', 'By:Og Mandino',new Date(2018,7,9)),
+    new Quote(1, '"Sometime we can feel a bit dull in the morning and we need to be our own sunshine energy"', 'By:Johan McGinn',new Date(2018,7,6)),
+    new Quote(2, '"Good,better,best.Never let it rest.Till your good is better and your better is best"', 'By:St.Jerome',new Date(2018,7,7)),
+    new Quote(3, '"Create the highest vision possible for your life,because you become what you believe"', 'By:Owen Muriithi',new Date(2018,7,8)),
+    new Quote(4, '"Failure will never overtake me if my determination to succeed is strong enough"', 'By:Og Mandino',new Date(2018,7,9)),
   ]
   
   deleteQuote(isComplete,index){
     if (isComplete){
-      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
       
       if(toDelete){
         this.quotes.splice(index,1)
